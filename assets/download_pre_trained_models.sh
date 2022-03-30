154f42aba4c8b3094fde623d58f3574430d9203c
hibetterheyj <he-yujie@outlook.com>
2022-03-30 08:31:18 +0200

    add output folder to downloading script

--- assets/download_pre_trained_models.sh
+++ assets/download_pre_trained_models.sh
@@ -1,2 +1,2 @@
 conda activate dense_matching_env
-gdown --folder https://drive.google.com/drive/folders/1LVrwAHDvVxsqzaGtd409wHv8cSPeLj1i
+gdown --folder https://drive.google.com/drive/folders/1LVrwAHDvVxsqzaGtd409wHv8cSPeLj1i -O ./pre_trained_models
